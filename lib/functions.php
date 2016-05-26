98201bad5df87f46f0c0e4d72eb8c13fb39455f0
Jes Constantine <jesconstantine@gmail.com>
2016-05-26 13:36:21 -0400

    add individual pet profile page with show_pet function

--- lib/functions.php
+++ lib/functions.php
@@ -34,6 +34,18 @@ function get_pets($limit = null) {
   return $pets;
 }
 
+function get_pet($id) {
+  $pdo = get_connection();
+  // address having variable in sql query (vulnerable to injection IE ; TRUNCATE pet;) by using prepared statements
+  $query = 'SELECT * FROM pet WHERE id = :idVal';
+  $stmt = $pdo->prepare($query); // returns pdo object
+  $stmt->bindParam('idVal', $id); // bind $id to idVal
+  $stmt->execute();
+
+  return $stmt->fetch();
+
+}
+
 function save_pets($petsToSave) {
   //d($petsToSave); die;
   $json = json_encode($petsToSave, JSON_PRETTY_PRINT);
